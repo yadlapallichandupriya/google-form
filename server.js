c538f6140be28c4a36c7d54d775542f54b8d6166
yadlapallichandupriya <chanduyadlapalli2004@gmail.com>
2025-02-05 07:48:20 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, () => console.log("Server running at http://localhost:3000"));
+
